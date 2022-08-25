2f0f4ce8fe76c65a2fb06065c9facf59a21330d9
xudashuai <xudashuai0819@outlook.com>
2022-08-25 19:43:22 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import Express from "express";
 import { MongoClient } from "mongodb";
 const app = Express();
 
-const uri = "mongodb://mongo:27017/";
+const uri = "mongodb://admin:admin@mongo:27017/";
 const dbClient = await new MongoClient(uri).connect();
+
 app.get("/getAllCategories", (req, res) => {
   res.json(dbClient.db("test").collection("categories").find().toArray());
 });
+
 app.listen(8080);
